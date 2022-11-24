92d0f461ee54ac9a145c5db7b38fd0f17eb78e52
colleg <colleg@cc.psu>
2022-11-24 16:28:37 +0300

    Добавлен файл main.py цифры

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     s1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    s3 = ['1','2','3','4','5','6','7','8','9','0']
     result = ''
     for i in range(11):
-        result += random.choice(s1)
+        result += random.choice(s1+s3)
     return result
 
 p = password()
